329e20a46f2cc7598e5c8b8cb6a43913777a77b8
The Trying Times <thetryingtimes@proton.me>
2025-02-09 18:37:47 -0700

    update loading & revert `data-inventory-after`

--- src/lib/components/articles/Article.svelte
+++ src/lib/components/articles/Article.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-  import type { Snippet } from 'svelte';
-
-  let {
-    children,
-    inventory_eligible = false
-  }: { children: Snippet; inventory_eligible?: boolean } = $props();
+  let { children } = $props();
 </script>
 
 <article
   class="prose prose-headings:font-serif prose-headings:font-light prose-headings:mb-0 m-auto max-w-prose px-4 pt-4 pb-8"
-  data-inventory-after={inventory_eligible || undefined}
 >
   {@render children()}
 </article>
--- src/routes/(api)/-/article/[external_id]/+server.ts
+++ src/routes/(api)/-/article/[external_id]/+server.ts
@@ -2,6 +2,7 @@ import type { GetCachedArticleResponse } from '$lib/api/kv/article';
 import { KVClient } from '$lib/server/utils/kv';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { SupabaseClient } from '$lib/server/utils/supabase';
 
 export const GET: RequestHandler = async ({ platform, params }) => {
   const failure: GetCachedArticleResponse = { success: false };
@@ -16,6 +17,32 @@ export const GET: RequestHandler = async ({ platform, params }) => {
       success: true,
       cached_article
     } as GetCachedArticleResponse);
+  else {
+    const supa = new SupabaseClient(platform);
+    const { data } = await supa.adminGetArticleByExternalId(params.external_id);
+
+    if (data && data[0]) {
+      return json({
+        success: true,
+        cached_article: {
+          article: {
+            external_id: data[0].external_id,
+            published_at: data[0].published_at,
+            published: data[0].published,
+            category_id: data[0].category_id,
+            voting_enabled: data[0].voting_enabled,
+
+            content: data[0].content
+          },
+          votes: {
+            support: 0,
+            oppose: 0,
+            shares: 0
+          }
+        }
+      } as GetCachedArticleResponse);
+    }
+  }
 
   return json(failure);
 };
