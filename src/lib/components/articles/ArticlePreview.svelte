3d97652dd874260454e93b320da87b7337bb2e92
The Trying Times <thetryingtimes@proton.me>
2025-02-11 14:12:46 -0700

    optimization: add io to load article previews

--- src/lib/components/articles/ArticlePreview.svelte
+++ src/lib/components/articles/ArticlePreview.svelte
@@ -15,7 +15,10 @@
   let error = $state(false);
   let cached_article: CachedArticle | undefined = $state();
 
-  onMount(async () => {
+  let root: HTMLDivElement;
+  let io: IntersectionObserver;
+
+  const load = async () => {
     const res = await ApiClient.get<GetCachedArticleResponse>(
       getCachedArticleEndpoint(external_id),
       GetCachedArticleResponseParser
@@ -29,42 +32,56 @@
     if (!cached_article) {
       error = true;
     }
+  };
+
+  onMount(async () => {
+    io = new IntersectionObserver(async (entries) => {
+      const entry = entries[0];
+      if (entry && entry.isIntersecting) {
+        io.disconnect();
+        await load();
+      }
+    });
+
+    io.observe(root);
   });
 </script>
 
-{#if loading && !error}
-  <div class="px-4">
-    <div class="flex flex-col gap-2 overflow-hidden">
-      <div class="h-20 animate-pulse bg-gray-300"></div>
-      <div class="animation-delay-med h-12 animate-pulse bg-gray-300"></div>
-      <div class="animation-delay-long h-8 animate-pulse bg-gray-300"></div>
-    </div>
-  </div>
-{:else if error}{:else if cached_article}
-  <article class="flex flex-col gap-4 px-4">
-    <div>
-      <ArticleHeader bind:cached_article />
-      <div class="group">
-        <h2 class="mt-2">
-          <a
-            href={`/${cached_article.article.external_id}`}
-            class="font-serif text-2xl text-pretty md:text-3xl"
-            >{cached_article.article.content.title}</a
-          >
-        </h2>
-        <p class="mt-1">
-          <a
-            href={`/${cached_article.article.external_id}`}
-            class="text-pretty"
-          >
-            {cached_article.article.content.blurb}&nbsp;<span
-              class="font-cond text-ttt whitespace-nowrap uppercase group-hover:underline"
-              >Read &gt;&gt;</span
-            >
-          </a>
-        </p>
+<div bind:this={root}>
+  {#if loading && !error}
+    <div class="px-4">
+      <div class="flex flex-col gap-2 overflow-hidden">
+        <div class="h-20 animate-pulse bg-gray-300"></div>
+        <div class="animation-delay-med h-12 animate-pulse bg-gray-300"></div>
+        <div class="animation-delay-long h-8 animate-pulse bg-gray-300"></div>
       </div>
     </div>
-    <ArticleNav bind:cached_article />
-  </article>
-{/if}
+  {:else if error}{:else if cached_article}
+    <article class="flex flex-col gap-4 px-4">
+      <div>
+        <ArticleHeader bind:cached_article />
+        <div class="group">
+          <h2 class="mt-2">
+            <a
+              href={`/${cached_article.article.external_id}`}
+              class="font-serif text-2xl text-pretty md:text-3xl"
+              >{cached_article.article.content.title}</a
+            >
+          </h2>
+          <p class="mt-1">
+            <a
+              href={`/${cached_article.article.external_id}`}
+              class="text-pretty"
+            >
+              {cached_article.article.content.blurb}&nbsp;<span
+                class="font-cond text-ttt whitespace-nowrap uppercase group-hover:underline"
+                >Read&nbsp;&gt;&gt;</span
+              >
+            </a>
+          </p>
+        </div>
+      </div>
+      <ArticleNav bind:cached_article />
+    </article>
+  {/if}
+</div>
