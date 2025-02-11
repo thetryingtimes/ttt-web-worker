3ac71af9a6483c2fc39de576a35ac30d4c132818
The Trying Times <thetryingtimes@proton.me>
2025-02-11 14:34:44 -0700

    fresh-load if no cached article (local dev)

--- src/routes/(app)/[article_external_id]/+page.server.ts
+++ src/routes/(app)/[article_external_id]/+page.server.ts
@@ -1,6 +1,10 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { type CachedArticle } from '$lib/api/kv/article';
+import {
+  type CachedArticle,
+  type GetCachedArticleResponse
+} from '$lib/api/kv/article';
+import { SupabaseClient } from '$lib/server/utils/supabase';
 
 export const load: PageServerLoad = async ({ platform, params }) => {
   if (!platform) error(404);
@@ -10,7 +14,35 @@ export const load: PageServerLoad = async ({ platform, params }) => {
     'json'
   );
 
-  if (!cached_article) error(404);
+  if (!cached_article) {
+    const supa = new SupabaseClient(platform);
+    const { data } = await supa.adminGetArticleByExternalId(
+      params.article_external_id
+    );
+
+    if (data && data[0]) {
+      return {
+        cached_article: {
+          article: {
+            external_id: data[0].external_id,
+            published_at: data[0].published_at,
+            published: data[0].published,
+            category_id: data[0].category_id,
+            voting_enabled: data[0].voting_enabled,
+
+            content: data[0].content
+          },
+          votes: {
+            support: 0,
+            oppose: 0,
+            shares: 0
+          }
+        }
+      } as { cached_article: CachedArticle };
+    } else {
+      error(404);
+    }
+  }
 
   return {
     cached_article
